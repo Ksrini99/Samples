fc941d1017cb5a73151577a7d12b3a0c6e7a0883
Ksrini99! <kongarasrini9@gmail.com>
2022-07-13 19:41:51 +0530

    git commit;

---
+++ src/com/srini/samples/Time.java
@@ -0,0 +1,14 @@
+package com.srini.samples;
+
+public class Time {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
